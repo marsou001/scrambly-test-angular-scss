796a7f64daeaaaceae79c947ae2b866eab00066f
marouane <marsou431@gmail.com>
2022-11-29 12:11:16 +0100

    feat: reviews trustpilot

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { HeaderModule } from './header/header.module';
 import { ActionsBarComponent } from './actions-bar/actions-bar.component';
 import { HeroComponent } from './hero/hero.component';
+import { ReviewsComponent } from './reviews/reviews.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ActionsBarComponent,
     HeroComponent,
+    ReviewsComponent,
   ],
   imports: [
     BrowserModule,
