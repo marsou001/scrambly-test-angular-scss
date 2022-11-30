22350a44fef2a1fa2a433dfdefb2d839b875c8c8
marouane <marsou431@gmail.com>
2022-11-30 11:56:01 +0100

    style: switch text with icons actions-bar

--- src/app/actions-bar/actions-bar.module.ts
+++ src/app/actions-bar/actions-bar.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActionsBarComponent } from './actions-bar.component';
 
-
-
 @NgModule({
   declarations: [ActionsBarComponent],
   exports: [ActionsBarComponent],
