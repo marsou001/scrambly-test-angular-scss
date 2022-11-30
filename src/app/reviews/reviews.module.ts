81626174431aaae16d6c28853e83bde7b44144f3
marouane <marsou431@gmail.com>
2022-11-30 12:43:47 +0100

    refactor: tidy up scss reviews

--- src/app/reviews/reviews.module.ts
+++ src/app/reviews/reviews.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReviewsComponent } from './reviews.component';
 
-
-
 @NgModule({
   declarations: [ReviewsComponent],
   exports: [ReviewsComponent],
